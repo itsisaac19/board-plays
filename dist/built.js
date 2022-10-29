cf7ba8370e0feec87a3b5286f97fdabfec465811
Isaac Tsai <64609807+itsisaac19@users.noreply.github.com>
2022-10-28 19:33:40 -0500

    Insecure Content, JS

---
+++ dist/built.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const initPage = () => {\r\n    let btn = document.querySelector('.btn');\r\n    btn.onclick = () => {\r\n        location.href = 'board.html';\r\n    }\r\n}\r\ninitPage();\r\n\r\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://ultimatevisual/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
