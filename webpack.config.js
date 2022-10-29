0e697daac5fdb4d5964a0cef6e830301c8925cd4
Isaac Tsai <64609807+itsisaac19@users.noreply.github.com>
2022-10-28 19:36:22 -0500

    Restructure

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, '/public/scripts')
     },
     mode: 'development',
     watch: true,
